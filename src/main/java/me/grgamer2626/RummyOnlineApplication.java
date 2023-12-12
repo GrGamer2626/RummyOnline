ddaba8e65ca5cf4faf3cfd166e3f97db0c3c098e
GrGamer2626 <g2626@tlen.pl>
2023-12-12 18:15:11 +0100

    Initial project configuration
    Configured .gitignore file, established project structure using Maven,
    and added basic setup with necessary dependencies in pom.xml:
    
    - MySQL database driver
    - Spring Boot frameworks with Spring Data JPA for database handling
    - Spring Security for user authentication and authorization
    - Spring Web Socket for real-time server-user communication
    - Spring Validation for validating user input data
    - Spring Test for unit testing
    - Thymeleaf for dynamic HTML file generation

---
+++ src/main/java/me/grgamer2626/RummyOnlineApplication.java
@@ -0,0 +1,21 @@
+package me.grgamer2626;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class RummyOnlineApplication {
+
+	private static ApplicationContext applicationContext;
+	
+	public static void main(String[] args) {
+		applicationContext =  SpringApplication.run(RummyOnlineApplication.class, args);
+	}
+	
+	public static synchronized ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
+}
