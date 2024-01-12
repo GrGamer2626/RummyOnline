6073ac1c723642e2a62ba77355a5fd758c914f76
GrGamer2626 <g2626@tlen.pl>
2024-01-12 17:32:10 +0100

    Enhance Page Access Control
    Limited access to login, registration, and forget password pages for
    logged-in users.
    Configured redirection in case of access denial.

--- src/main/java/me/grgamer2626/configuration/SecurityConfig.java
+++ src/main/java/me/grgamer2626/configuration/SecurityConfig.java
@@ -5,17 +5,22 @@ import me.grgamer2626.model.users.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.access.AccessDeniedHandlerImpl;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,7 +29,7 @@ import java.util.stream.Collectors;
 @EnableWebSecurity
 public class SecurityConfig {
 	
-	private final String[] permitAll = {"/css/**", "/js/**", "/img/**", "/", "/login", "/registration", "/forget-password", "/about-us", "how-to-play", "/rules", "/contact" };
+	private final String[] permitAll = {"/css/**", "/js/**", "/img/**", "/",  "/about-us", "how-to-play", "/rules", "/contact"};
 	private final UserRepository userRepository;
 	
 	@Autowired
@@ -36,11 +41,13 @@ public class SecurityConfig {
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(requests -> (requests
+		http.authorizeHttpRequests(requests -> requests
 						.requestMatchers(permitAll).permitAll()
+						.requestMatchers("/login", "/registration", "/forget-password").anonymous()
 						.requestMatchers("/admin").hasAnyRole("ADMIN")
-						.anyRequest())
+						.anyRequest()
 				.authenticated())
+				
 				
 				.formLogin(form -> form
 						.loginPage("/login")
@@ -50,10 +57,29 @@ public class SecurityConfig {
 				
 				.logout(logout-> logout.logoutSuccessUrl("/"))
 				
+				.exceptionHandling(exception-> exception.accessDeniedHandler(accessDeniedHandler()))
 				.httpBasic(Customizer.withDefaults());
 		
 		return http.build();
 	}
+	
+	@Bean
+	public AccessDeniedHandler accessDeniedHandler() {
+		return (request, response, accessDeniedException) -> {
+			if (response.isCommitted()) {
+				return;
+			}
+			Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+			boolean logged = auth != null && auth.isAuthenticated() && !(auth instanceof AnonymousAuthenticationToken);
+			
+			if(logged) {
+				response.sendRedirect("/lobby");
+			} else {
+				response.sendRedirect("/");
+			}
+		};
+	}
+	
 	@Bean
 	public DaoAuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider authProvider =  new DaoAuthenticationProvider(getPasswordEncoder());
@@ -77,6 +103,7 @@ public class SecurityConfig {
 			return new org.springframework.security.core.userdetails.User(user.getName(), user.getPassword(), authorityUtils );
 		};
 	}
+
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		int strength = 10; //the higher, the safer but longer time
