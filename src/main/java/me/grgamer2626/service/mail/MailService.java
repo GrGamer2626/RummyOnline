d9ba452a43d2f74aadd9f81d8f46dd9f9cd8b228
GrGamer2626 <g2626@tlen.pl>
2024-01-25 20:39:49 +0100

    Move Token Generation Methods
    Transferred token generation methods from MailService to
    VerificationTokenService

--- src/main/java/me/grgamer2626/service/mail/MailService.java
+++ src/main/java/me/grgamer2626/service/mail/MailService.java
@@ -4,7 +4,6 @@ import jakarta.mail.MessagingException;
 import me.grgamer2626.model.users.User;
 
 import java.io.UnsupportedEncodingException;
-import java.util.UUID;
 
 public interface MailService {
 	
@@ -12,16 +11,4 @@ public interface MailService {
 	
 	String createVerificationUrl(String applicationUrl, String verificationToken);
 	
-	 default UUID generateUUID() {
-		 return UUID.randomUUID();
-	 }
-	
-	 default UUID generateUUID(String key, String userEmail) {
-		 String tokenBase = key + ":" + userEmail;
-		 
-		 return UUID.nameUUIDFromBytes(tokenBase.getBytes());
-	 }
-
-
-
 }
