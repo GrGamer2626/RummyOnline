eef2e6ebccd40a49de6f9fb19d50d31f15d41b5c
GrGamer2626 <g2626@tlen.pl>
2024-01-25 20:30:38 +0100

    Add getByUser Method
    Implemented getByUser method in VerificationTokenRepository

--- src/main/java/me/grgamer2626/model/users/token/VerificationTokenRepository.java
+++ src/main/java/me/grgamer2626/model/users/token/VerificationTokenRepository.java
@@ -1,10 +1,15 @@
 package me.grgamer2626.model.users.token;
 
+import me.grgamer2626.model.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface VerificationTokenRepository extends JpaRepository <VerificationToken, Long> {
+	
 	VerificationToken findByToken(String token);
+	
+	VerificationToken findByUser(User user);
+	
 }
