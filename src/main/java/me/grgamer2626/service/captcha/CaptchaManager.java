10b1ff28bdd3966c19010cca6a93c432a7fd6b44
GrGamer2626 <g2626@tlen.pl>
2024-01-28 02:51:06 +0100

    Update Captcha Service
    Set up configuration class and bean for RestTemplate. Implemented
    validateToken method in CaptchaService.

--- src/main/java/me/grgamer2626/service/captcha/CaptchaManager.java
+++ src/main/java/me/grgamer2626/service/captcha/CaptchaManager.java
@@ -4,17 +4,23 @@ package me.grgamer2626.service.captcha;
 import me.grgamer2626.utils.dto.captcha.CaptchaResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CaptchaManager implements CaptchaService {
 	
+	private final RestTemplate restTemplate;
 	private final String verifyUrl;
 	private final String secretKey;
 	private final String siteKey;
 	
 	@Autowired
-	public CaptchaManager(@Value("${hcaptcha.secret-key}") String secretKey, @Value("${hcaptcha.site-key}") String siteKey) {
+	public CaptchaManager(RestTemplate restTemplate, @Value("${hcaptcha.secret-key}") String secretKey, @Value("${hcaptcha.site-key}") String siteKey) {
+		this.restTemplate = restTemplate;
 		this.verifyUrl = "https://api.hcaptcha.com/siteverify";
 		this.secretKey = secretKey;
 		this.siteKey = siteKey;
@@ -24,8 +30,18 @@ public class CaptchaManager implements CaptchaService {
 	
 	@Override
 	public CaptchaResponse validateToken(String captchaToken) {
-
-		return null;
+		HttpHeaders headers =  new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		
+		MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+		map.add("secret", secretKey);
+		map.add("response", captchaToken);
+		
+		HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(map, headers);
+		
+		ResponseEntity<CaptchaResponse> response = restTemplate.exchange(verifyUrl, HttpMethod.POST, entity, CaptchaResponse.class);
+		
+		return response.getBody();
 	}
 	
 	
